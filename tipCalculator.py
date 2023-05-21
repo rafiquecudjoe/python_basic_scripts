ea1549d33c429b2e232cc00384f853bb2bff5b60
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2023-05-21 13:12:43 +0000

    initial commit

---
+++ tipCalculator.py
@@ -0,0 +1,17 @@
+
+
+print("Starting tip calculator program")
+
+food_charge = input("Enter food charge ")
+print(f'Food charge is ${food_charge}')
+
+eighteen_percent = (18/100)
+seven_percent = (7/100) 
+
+tip = float(food_charge) * eighteen_percent
+print(f'Tip = ${tip}')
+sales_tax = float(food_charge) * seven_percent
+print(f'Sales tax is ${sales_tax}')
+total_charge = tip + sales_tax + float(food_charge)
+print(f'Grand total = ${total_charge}')
+
